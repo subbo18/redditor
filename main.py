9bb1f8ca474c00f14531dd9ff79e844cd13e137c
subatomic <70423273+subbo18@users.noreply.github.com>
2023-11-24 18:42:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
+import discord
+import random
 from discord.ext import commands
+
+import asyncpraw
+
+#BOT TOKEN, DO NOT SHARE
 TOKEN = "bot token"
 
-# Initialize Bot and Denote The Command Prefix
-bot = commands.Bot(command_prefix="/")
+#schniippppp reddit account info shit
+reddit = asyncpraw.Reddit(
+    client_id="bla",
+    client_secret="bla",
+    password = "bla",
+    user_agent="bla",
+    username = "bla",
+)
 
-# Runs when Bot Succesfully Connects
+#creates comman prefix, idk what the other shit does
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='schnip ',intents=intents)
+
+#runs when bot successfully connects
 @bot.event
 async def on_ready():
     print(f'{bot.user} succesfully logged in!')
 
-bot.run(TOKEN)
-
-async def response(message):
-    if  message.author == bot.User:
+@bot.event
+async def on_message(message):
+    #makes sure bot doesnt respond to own mesages
+    if message.author == bot.user: 
         return
-    
-    if message.content == "test":
-        await message.channel.send(f'{message.author} hasnt showered since kurt cobain died')
-
+    #schnip :3
+    #1/10 chance of responding with :3
+    probs = random.randint(1, 10)
+    if message.content == 'schnip':
+        if probs == 3:
+            await message.channel.send(f':3')
+        else:
+            await message.channel.send(f'schnip')
     await bot.process_commands(message)
+
+@bot.command()
+#lists top ten posts of subreddit
+#ctx refers to Commands.context object
+async def topten(ctx, sub):
+    print(sub)
+    async for submission in reddit.subreddit(sub).top(limit=10):
+        await ctx.send(submission.title)
 
 bot.run(TOKEN)
