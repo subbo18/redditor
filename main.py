97527c987354a9279fe84882626aca0b2172bd34
subatomic <70423273+subbo18@users.noreply.github.com>
2023-11-24 23:34:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ reddit = asyncpraw.Reddit(
     username = "bla",
 )
 
-#creates comman prefix, idk what the other shit does
+#creates command prefix, idk what the other shit does
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='schnip ',intents=intents)
 
@@ -41,11 +41,34 @@ async def on_message(message):
     await bot.process_commands(message)
 
 @bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+#array for retrieving url function
+numarr = []
+
+@bot.command()
 #lists top ten posts of subreddit
 #ctx refers to Commands.context object
 async def topten(ctx, sub):
-    print(sub)
-    async for submission in reddit.subreddit(sub).top(limit=10):
-        await ctx.send(submission.title)
-
+    array = []
+    subreddit = reddit.subreddit(sub)
+    for submission in subreddit.top(limit=10):
+        array.append(submission)
+    int = 0
+    for i in array:
+        int = int+1
+        numarr.append(i)
+        #url is sends affiliated media (?) doesnt work for galleries or videos
+        #if no image it just sends the plain url of the post
+        await ctx.send(str(int) + ". " + i.title + " " + i.url)
+    
+@bot.command()
+async def link(ctx, num):
+    #removes first ten elements from the previous query
+    n = 10
+    newlist = numarr[n:]
+    #THIS ISNT THE URL FUNCTION, need fix
+    await ctx.send(newlist[int(num)-1].url)
+        
 bot.run(TOKEN)
